073df605e8e6deb3aefb2933df0c8722dbfcc2bc
Stefan Gloutnikov <stefan@gloutnikov.com>
2019-05-20 21:07:28 -0700

    Fixed login due to changed HTML element

--- app/src/main/java/co/bgcs/neterraproxy/NeterraProxy.java
+++ app/src/main/java/co/bgcs/neterraproxy/NeterraProxy.java
@@ -246,8 +246,9 @@ class NeterraProxy extends NanoHTTPD {
                 .build();
         try {
             okhttp3.Response response = client.newCall(getRequest).execute();
+            //TODO: Pull token from <script> tag with regex?
             Document loginPageDoc = Jsoup.parse(response.body().string());
-            token = loginPageDoc.getElementById("wrapper").selectFirst("input[name=_token]")
+            token = loginPageDoc.getElementById("js--sign-in").selectFirst("input[name=_token]")
                     .attr("value");
         } catch (IOException e) {
             e.printStackTrace();
